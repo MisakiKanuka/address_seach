0b1e651fc0a8ba4bd44a9cac6ae6c3880b3ce60d
Kanuka <kanukamisaki@icloud.com>
2018-11-24 15:15:11 +0900

    八幡平市大更の情報をrequestaモジュールを使って取得する

--- request_sample01.py
+++ request_sample01.py
@@ -1,3 +1,11 @@
 import requests
 
-print(requests)
+responce = requests.get(url="http://hachimantai.spartacamp.jp/")
+
+print(responce)
+
+# 八幡平市大更の情報をrequestaモジュールを使って取得してね
+responce = requests.get(url="http://zipcloud.ibsnet.co.jp/api/search?zipcode=0287111")
+
+# 演習２
+responce = requests.get(url="http://zipcloud.ibsnet.co.jp/api/search?zipcode=0200757")
