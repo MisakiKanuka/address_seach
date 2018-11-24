aeca7b239892a1bf7fe1bf56fa82d532fbfaf9aa
Kanuka <kanukamisaki@icloud.com>
2018-11-24 17:22:18 +0900

    url = f"http://zipcloud.ibsnet.co.jp/api/search?zipcode={postal_code}"
    に変更
    
    スペルミスのチェック大事！

--- test_address_seacher.py
+++ test_address_seacher.py
@@ -1,16 +1,32 @@
 import unittest
+import requests
 
 
 class AddressSearcher:
     def search(self, postal_code):
-        return "岩手県八幡平市大更"
+        url = f"http://zipcloud.ibsnet.co.jp/api/search?zipcode={postal_code}"
+
+        responce = requests.get(url)
+
+        responce_dict = responce.json()
+
+        都道府県 = responce_dict["results"][0]["address1"]
+        市区町村 = responce_dict["results"][0]["address2"]
+        町域 = responce_dict["results"][0]["address3"]
+
+        return f"{都道府県}{市区町村}{町域}"
+
+        # responce.json()
+        # ["results"][0]["address2"]
+
+        # return "岩手県八幡平市大更"
 
 
 class TestAddressSearcher(unittest.TestCase):
     def test_岩手県八幡平市大更の地名を郵便番号から取得できる(self):
         address_searcher = AddressSearcher()
 
-        actual = address_searcher.search(postal_code="02807111")
+        actual = address_searcher.search(postal_code="0287111")
 
         self.assertEqual("岩手県八幡平市大更", actual)
 
@@ -20,8 +36,6 @@ class TestAddressSearcher(unittest.TestCase):
         actual = address_searcher.search(postal_code="1760014")
 
         self.assertEqual("東京都練馬区豊玉南", actual)
-
-
 
 
 if __name__ == "__main__":
